ec8b32f8a9b7a74a4b21d25f77c3a61a004fa23e
Hyuna917 <hyuna917@me.com>
2015-05-24 22:37:32 +0900

    1st

---
+++ global.f90
@@ -0,0 +1,16 @@
+module global 
+    use kind_type 
+    implicit none
+    integer (i1), parameter :: file_input = 5, file_log = 6 
+    real    (dp), save :: Amp, Mass, Charge, Spin, Scatt, Bound, dr_p_drho
+    integer (i4), save :: L, N, F, M, pr, ptheta 
+    real    (dp), save, allocatable :: &
+        coord_rho(:), coord_weight(:), coord_dshape(:, :), &
+        H(:, :), E(:), R(:), K(:)
+    complex (dp), save, allocatable :: S(:, :), A(:)
+    character(1), save :: &
+        op_ev, op_degree, op_aa, & 
+        op_basis, op_bound, & 
+        op_dcs, op_inner, op_outer, &
+        op_tcs, op_ps
+end module
